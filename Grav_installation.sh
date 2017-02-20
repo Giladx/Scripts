cf165bc83afa10fb8fdaf190f8fc7e69b7b89046
Gilad Levi <gilad.x@gmail.com>
2017-02-20 02:01:50 +0200

    grav cms flat install script
    run from Grav zip archive location (cPanel 62.0 and upper)

---
+++ Grav_installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Program:
+#   grav instllation
+# History:
+# 01-04-2017 giladlevi First release.
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+unzip grav-v1.1.*&&
+echo "extract done."&&
+mv grav/ ~/public_html/&&
+mv -v ~/public_html/grav/* ~/public_html/&&
+rm -R ~/public_html/grav/&&
+echo "Grav CMS is Installed. Installing Admin Panel ..."&&
+cd ~/public_html&&
+bin/gpm install admin&&
+echo "Grav Admin is installed"&&
+echo "all finished"
+

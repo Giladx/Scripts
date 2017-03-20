6273b9f4e6dcfd004d7c9521ecb13e3b90a241b6
giladx <gilad.x@gmail.com>
2017-03-20 02:10:20 +0200

    fixed ones

--- grav_installation.sh
+++ grav_installation.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 # Program:
-#   grav instllation
-# History:
+# grav instllation
+# History: v1.0
 # 01-04-2017 giladlevi First release.
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
 
-unzip grav-v1.1.*&&
-echo "extract done."&&
-mv grav/ ~/public_html/&&
-mv -v ~/public_html/grav/* ~/public_html/&&
-rm -R ~/public_html/grav/&&
-echo "Grav CMS is Installed. Installing Admin Panel ..."
+unzip grav-*&&
+echo "extract done."
+mv grav/ ~/public_html/
+shopt -s dotglob
+mv -v ~/public_html/grav/* ~/public_html/
+rm -R ~/public_html/grav/
+chmod a+x adminscript.sh
+chmod a+x particles.sh
+cp plugins_script.sh ~/public_html/
+cp particales_and_atoms.sh ~/public_html/user/data/
+cd ~/public_html/
+echo -e "\e[92mdone"
+echo -e "\e[92mnow do \e[96mcd ~/public_html \e[92mand run plugins_script.sh"
--- plugins_script.sh
+++ plugins_script.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # Program:
-#   grav instllation
+# plugins installation
 # History:
 # 01-04-2017 giladlevi First release.
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
-
+# v1.0
+echo
 bin/gpm install admin
 echo
 echo installing gantry
 bin/gpm install gantry5
-echo installing themes
+echo -e "\e[92minstalling themes"
 bin/gpm install g5_hydrogen
 echo
 bin/gpm install g5_helium
-# Add more plugins here ...
+echo
+bin/gpm install optimus shoppingcart-paypal shoppingcart adminidenticons login-oauth
+echo -e "\e[32mYou can add more plugins at the end of this script ..."
+echo -e "\e[31mBye \e[5mBye \e[39m;) !"
+echo
